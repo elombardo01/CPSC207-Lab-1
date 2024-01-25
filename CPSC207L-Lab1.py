f24a93f9ba599a080ea4955148c58434ddcf57e2
elombardo01 <157068490+elombardo01@users.noreply.github.com>
2024-01-25 16:10:43 -0500

    Add files via upload

---
+++ CPSC207L-Lab1.py
@@ -0,0 +1,114 @@
+Python 3.12.1 (v3.12.1:2305ca5144, Dec  7 2023, 17:23:38) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+#EmmeLombardo
+#Therese
+#Allison
+print("Hello, world")
+Hello, world
+print("Welcome to CPSC 207")
+Welcome to CPSC 207
+print("Hello\nWorld")
+Hello
+World
+print("How\nare\nyou)
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print("How\nare\nyou?")
+      
+How
+are
+you?
+print("Today", "is", "Wednesday")
+      
+Today is Wednesday
+print("Today" + "is" + "Wednesday")
+      
+TodayisWednesday
+#print spits out what we type
+      
+# \n creates a new line for the statement
+      
+# The plus sign puts the strings next to each other
+      
+#The first predict is going to have a b c one line for each letter.
+      
+#The second predict will have hello in the first line and world! in the second
+      
+#The third predict will start on the second line with Hello and the third line will have World!
+      
+print("a\nb\nc")
+      
+a
+b
+c
+print("Hello" + "World")
+      
+HelloWorld
+print("Hello\n" +"\nWorld1")
+      
+Hello
+
+World1
+print("123\n45\n6\n")
+      
+123
+45
+6
+
+1+2*3
+      
+7
+(1+2)*3
+      
+9
+7/3
+      
+2.3333333333333335
+7//3
+      
+2
+7%3
+      
+1
+>>> 22/4
+...       
+5.5
+>>> 22//4
+...       
+5
+>>> 22%4
+...       
+2
+>>> 3**2
+...       
+9
+>>> 2**3
+...       
+8
+>>> # Yes, python does follow the usual order of operations
+...       
+>>> #/ norally devides and // divides but gets ride of fraction (makes the value an integer)
+...       
+>>> # % Gives us the value of the remainder after dividing
+...       
+>>> # ** is just exponential
+...       
+>>> # first prediction will give us 2
+...       
+>>> # second prediction will give us 0
+...       
+>>> (4+3)//2
+...       
+3
+>>> 6%2
+...       
+0
+>>> 1+2*3**4
+...       
+163
+>>> print("2+2=4")
+...       
+2+2=4
+>>> print("50 pounds is", 50*0.453592, "Kilograms")
+...       
+50 pounds is 22.6796 Kilograms
